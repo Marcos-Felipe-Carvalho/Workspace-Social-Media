7a1ee76aa749de9c1482c011e172c0c179375012
Marcos Felipe Carvalho <marcosfelipec2001@gmail.com>
2021-03-15 17:50:22 -0300

    Implementada animações nos componentes login e register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { AuthenticationComponent } from './authentication/authentication.component';
 import { LoginComponent } from './authentication/login/login.component';
 import { BannerComponent } from './authentication/banner/banner.component';
+import { RegisterComponent } from './authentication/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AuthenticationComponent,
     LoginComponent,
-    BannerComponent
+    BannerComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
--- src/app/authentication/authentication.component.ts
+++ src/app/authentication/authentication.component.ts
@@ -1,15 +1,45 @@
+import { trigger, state, style, transition, animate } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-authentication',
   templateUrl: './authentication.component.html',
-  styleUrls: ['./authentication.component.scss']
+  styleUrls: ['./authentication.component.scss'],
+  animations: [
+    trigger('animation-banner', [
+      state('created', style({
+        opacity: 1
+      })),
+      transition('void => created', [
+        style({opacity: 0, transform: 'translate(50px,0px)'}),
+        animate('500ms 0s ease-in-out')
+      ])
+    ]),
+    trigger('animation-form', [
+      state('created', style({
+        opacity: 1
+      })),
+      transition('void => created', [
+        style({opacity: 0, transform: 'translate(-50px, 0px'}),
+        animate('500ms 0s ease-in-out')
+      ])
+    ])
+  ]
 })
 export class AuthenticationComponent implements OnInit {
+
+  public statusBanner: string = 'created'
+  public statusForm:string = 'created'
+
+  public register:boolean = false
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public showScreen(event:string):void{
+    this.register = event === 'register' ? true:false
   }
 
 }
